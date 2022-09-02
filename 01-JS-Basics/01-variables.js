9184fbbdf2118302471200a185f29a4c39c995ed
Janainateixeira <jmtannus@gmail.com>
2022-09-02 15:06:10 -0300

    Subindo versao incial

---
+++ 01-JS-Basics/01-variables.js
@@ -0,0 +1,14 @@
+
+////////////////////////
+//Abstrai a lapiseira//
+var diametroGrafite = 0.7; //number (float)
+var traco = 'fino'; //string (string)
+var grip = 'emborrachado'; //string (string)
+var material = 'metal'; //string (string)
+var durabilidade = 'duravel'; //string (string)
+var preco = 25; //number (inteiro) 
+var matPonteira = 'metal'; //string (string)
+var recursoExtra = 'avanço automatico'; //string (string)
+var sistemaAvanco = 'Botão traseiro'; //string (string)
+var marca = 'BIC Velocity Grip'; //string (string)
+var cor = 'azul'; //string (string)
